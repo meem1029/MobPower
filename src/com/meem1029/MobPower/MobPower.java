6a6f1ec132b714f2662fb2e8908dce1eac42feb5
Josh T (meem1029) <jtmeem@gmail.com>
2012-09-29 13:18:09 -0500

    Added Damage Adjustment

--- src/com/meem1029/MobPower/MobPower.java
+++ src/com/meem1029/MobPower/MobPower.java
@@ -1,9 +1,12 @@
 package com.meem1029.MobPower;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MobPower extends JavaPlugin{
@@ -12,7 +15,21 @@ public class MobPower extends JavaPlugin{
 	private Equation damageEquation;
 	
 	
+	// Damage Changing
+	@EventHandler
+	public void onMobDamage(EntityDamageByEntityEvent e){
+		Entity mob = e.getDamager();
+		Entity victim = e.getEntity();
+		if(mob instanceof Player){
+			return;
+		}
+		int oldDamage = e.getDamage();
+		double multiplier = damageEquation.getValue(getDistance(mob.getLocation()));
+		int newDamage = (int) (oldDamage * multiplier);
+		e.setDamage(newDamage);
+	}
 	
+	// Health Changing
 	@EventHandler
 	public void onMobSpawn(CreatureSpawnEvent e){
 		LivingEntity mob = e.getEntity();
