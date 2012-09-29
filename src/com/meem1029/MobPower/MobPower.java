065fb1fcfff62e8a8c168a922830041264742ec5
Josh T (meem1029) <jtmeem@gmail.com>
2012-09-29 13:25:54 -0500

    Filled in boilerplate code to make it run.

--- src/com/meem1029/MobPower/MobPower.java
+++ src/com/meem1029/MobPower/MobPower.java
@@ -1,19 +1,37 @@
 package com.meem1029.MobPower;
 
+import java.util.logging.Logger;
+
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MobPower extends JavaPlugin{
-	
+public class MobPower extends JavaPlugin implements Listener{
+
+	private Logger log;
 	private Equation healthEquation;
 	private Equation damageEquation;
 	
+	
+	public void onEnable(){
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this,this);
+		log = Logger.getLogger("Minecraft");
+		this.getEquations();
+	}
+	
+	//Will read from a config file in the future. For now due to time, they're hard coded.
+	private void getEquations(){
+		healthEquation = new LinearEquation(2000,1,1800,3.5);
+		damageEquation = new LinearEquation(2000,0,1600,1);
+	}
 	
 	// Damage Changing
 	@EventHandler
