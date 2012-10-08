b9749cb6f5a72cc80906f0f0817242bdf97df984
Josh T (meem1029) <jtmeem@gmail.com>
2012-10-08 14:22:30 -0500

    Moved the min/max functionality to LinearEquation. Added constructors including minimum and maximum values.

--- src/com/meem1029/MobPower/DropEquation.java
+++ src/com/meem1029/MobPower/DropEquation.java
@@ -7,27 +7,24 @@ public class DropEquation implements Equation<ItemStack> {
 
 	private Equation<Double> howMany;
 	private MaterialData mat;
-	private double max,min;
 	
 	public DropEquation(double x1, double y1, double x2, double y2, MaterialData mat){
 		howMany = new LinearEquation(x1,y1,x2,y2);
 		this.mat = mat;
-		this.min = 0;
-		this.max = Double.MAX_VALUE;
 	}
 	
 	public DropEquation(double x1, double y1, double x2, double y2, MaterialData mat, int min, int max){
-		howMany = new LinearEquation(x1,y1,x2,y2);
+		howMany = new LinearEquation(x1,y1,x2,y2,min,max);
 		this.mat = mat;
-		this.min = min;
-		this.max = max;
 	}
 	
-	
+	public DropEquation(Equation<Double> eq, MaterialData mat){
+		howMany = eq;
+		this.mat = mat;
+	}
 	
 	public ItemStack getValue(double x){
 		double num = howMany.getValue(x);
-		num = Math.max(min, Math.min(max,num));
 		int n = (int) Math.floor(num);
 		if(Math.random() < (num - n)){
 			n++;
--- src/com/meem1029/MobPower/LinearEquation.java
+++ src/com/meem1029/MobPower/LinearEquation.java
@@ -5,15 +5,28 @@ public class LinearEquation implements Equation<Double> {
 	private double x1;
 	private double y1;
 	private double m;
+	private double min;
+	private double max;
 	
 	public LinearEquation(double x1, double y1, double x2, double y2){
 		this.x1 = x1;
 		this.y1 = y1;
 		this.m = (y2 - y1)/(x2-x1);
+		this.min = -Double.MAX_VALUE;
+		this.max = Double.MAX_VALUE;
+	}
+	
+	public LinearEquation(double x1, double y1, double x2, double y2, double min, double max){
+		this.x1 = x1;
+		this.y1 = y1;
+		this.m = (y2 - y1)/(x2-x1);
+		this.min = min;
+		this.max = max;
 	}
 	
 	public Double getValue(double x){
-		return Double.valueOf(y1 + m * (x-x1));
+		double val = Double.valueOf(y1 + m * (x-x1));
+		return Math.max(min,Math.min(max, val));
 	}
 	
 }
