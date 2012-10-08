03a2707fb885bd408a439cc5609456f4de1b1f97
Josh T (meem1029) <jtmeem@gmail.com>
2012-10-08 14:02:07 -0500

    Added Max and Min options to DropEquation

--- src/com/meem1029/MobPower/DropEquation.java
+++ src/com/meem1029/MobPower/DropEquation.java
@@ -7,14 +7,27 @@ public class DropEquation implements Equation<ItemStack> {
 
 	private Equation<Double> howMany;
 	private MaterialData mat;
+	private double max,min;
 	
 	public DropEquation(double x1, double y1, double x2, double y2, MaterialData mat){
 		howMany = new LinearEquation(x1,y1,x2,y2);
 		this.mat = mat;
+		this.min = 0;
+		this.max = Double.MAX_VALUE;
 	}
+	
+	public DropEquation(double x1, double y1, double x2, double y2, MaterialData mat, int min, int max){
+		howMany = new LinearEquation(x1,y1,x2,y2);
+		this.mat = mat;
+		this.min = min;
+		this.max = max;
+	}
+	
+	
 	
 	public ItemStack getValue(double x){
 		double num = howMany.getValue(x);
+		num = Math.max(min, Math.min(max,num));
 		int n = (int) Math.floor(num);
 		if(Math.random() < (num - n)){
 			n++;
